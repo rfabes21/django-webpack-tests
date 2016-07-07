ff311971298a6b0a4869fb01be36ee055f156cc3
Adam Venturella <aventurella@blitzagency.com>
2016-07-07 15:22:13 -0700

    cleanup

--- django/demo/mysubapp/static/js/specs/vendor/init.js
+++
@@ -1,5 +0,0 @@
-define(function (require, exports, module) {
-    require('jasmine/jasmine-jquery');
-
-    jasmine.getFixtures().fixturesPath = '/base/project/static/js/specs/fixtures';
-});
--- django/demo/mysubapp/static/scss/vendor/README.md
+++
@@ -1,7 +0,0 @@
-# Built SCSS Deps
-
-## Versions
-
-- Bourbon: **v4.0.2**
-- Breakpoint: **v2.5.0**
-- Bootstrap: **3.3.4.1**
